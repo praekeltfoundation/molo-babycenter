905fb9481934da1b7ab6e45d4ac9f73112e9fdf2
TrevorOctober <trevor@praekeltconsulting.com>
2016-08-10 15:11:01 +0200

    added new languages

--- babycenter/settings/base.py
+++ babycenter/settings/base.py
@@ -204,6 +204,27 @@ LANGUAGES = global_settings.LANGUAGES + [
     ('ku', _('Kurdish')),
     ('arc', _('Aramaic')),
     ('ht', _('Creole Haitian')),
+    ('ar', _('Arabic')),
+    ('id', _('Bahasa')),
+    ('bn', _('Bengali')),
+    ('bur_MM', _('Burmese')),
+    ('fr', _('French')),
+    ('pt', _('Portuguese')),
+    ('es', _('Spanish')),
+    ('sw', _('Swahili')),
+    ('th', _('Thai')),
+    ('urd_PK', _('Urdu')),
+    ('kn', _('Kannada')),
+    ('ml', _('Malayalam')),
+    ('mr', _('Marathi')),
+    ('ne', _('Nepali')),
+    ('pa', _('Punjabi')),
+    ('ru', _('Russian')),
+    ('si', _('Sinhalese')),
+    ('sw', _('Swahili')),
+    ('ta', _('Tamil')),
+    ('tg', _('Telugu')),
+    ('tr', _('Turkish')),
 ]
 
 EXTRA_LANG_INFO = {
@@ -350,6 +371,150 @@ EXTRA_LANG_INFO = {
         'code': 'kr',
         'name': 'Creole Haitian',
         'name_local': 'Kreyòl ayisyen'
+    },
+    'ar': {
+        'bidi': False,
+        'code': 'ara_AE',
+        'name': 'Aribic',
+        'name_local': 'Arabic'
+    },
+    'id': {
+        'bidi': False,
+        'code': 'ind_ID',
+        'name': 'Bahasa',
+        'name_local': 'Bahasa'
+    },
+    'bn': {
+        'bidi': False,
+        'code': 'ben_IN',
+        'name': 'Bengali',
+        'name_local': 'Bengali'
+    },
+    'bur_MM': {
+        'bidi': False,
+        'code': 'bur_MM',
+        'name': 'Burmese',
+        'name_local': 'Burmese'
+    },
+    'fr': {
+        'bidi': False,
+        'code': 'fre_FR',
+        'name': 'French',
+        'name_local': 'French'
+    },
+    'pt': {
+        'bidi': False,
+        'code': 'por_PT',
+        'name': 'Portuguese',
+        'name_local': 'Portuguese'
+    },
+    'es': {
+        'bidi': False,
+        'code': 'spa_ES',
+        'name': 'Spanish',
+        'name_local': 'Spanish'
+    },
+    'es': {
+        'bidi': False,
+        'code': 'spa_AR',
+        'name': 'Spanish',
+        'name_local': 'Spanish'
+    },
+    'es': {
+        'bidi': False,
+        'code': 'spa_ES',
+        'name': 'Spanish',
+        'name_local': 'Spanish'
+    },
+    'es': {
+        'bidi': False,
+        'code': 'spa_ES',
+        'name': 'Spanish',
+        'name_local': 'Spanish'
+    },
+    'swa_KE': {
+        'bidi': False,
+        'code': 'swa_KE',
+        'name': 'Swahili',
+        'name_local': 'Swahili'
+    },
+    'th': {
+        'bidi': False,
+        'code': 'tha_TH',
+        'name': 'Thai',
+        'name_local': 'Thai'
+    },
+    'urd_PK': {
+        'bidi': False,
+        'code': 'urd_PK',
+        'name': 'Urdu',
+        'name_local': 'Urdu'
+    },
+    'kn': {
+        'bidi': False,
+        'code': 'kan_IN',
+        'name': 'Kannada',
+        'name_local': 'Kannada'
+    },
+    'ml': {
+        'bidi': False,
+        'code': 'mal_IN',
+        'name': 'Malayalam',
+        'name_local': 'Malayalam'
+    },
+    'mr': {
+        'bidi': False,
+        'code': 'mar_IN',
+        'name': 'Marathi',
+        'name_local': 'Marathi'
+    },
+    'ne': {
+        'bidi': False,
+        'code': 'nep_NP',
+        'name': 'Nepali',
+        'name_local': 'Nepali'
+    },
+    'pa': {
+        'bidi': False,
+        'code': 'pan_IN',
+        'name': 'Punjabi',
+        'name_local': 'Punjabi'
+    },
+    'ru': {
+        'bidi': False,
+        'code': 'rus_RU',
+        'name': 'Russian',
+        'name_local': 'Russian'
+    },
+    'si': {
+        'bidi': False,
+        'code': 'sin_LK',
+        'name': 'Sinhalese',
+        'name_local': 'Sinhalese'
+    },
+    'sw': {
+        'bidi': False,
+        'code': 'swa_TZ',
+        'name': 'Swahili',
+        'name_local': 'Swahili'
+    },
+    'ta': {
+        'bidi': False,
+        'code': 'tam_IN',
+        'name': 'Tamil',
+        'name_local': 'Tamil'
+    },
+    'tg': {
+        'bidi': False,
+        'code': 'tel_IN',
+        'name': 'Telugu',
+        'name_local': 'Telugu'
+    },
+    'tr': {
+        'bidi': False,
+        'code': 'tur_TR',
+        'name': 'Turkish',
+        'name_local': 'Turkish'
     },
 }
 
