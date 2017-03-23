41cd3357b596bf3bf518b3f95c9522c95174f3fc
nathanbegbie <nathanbegbie@gmail.com>
2017-03-23 13:03:18 +0200

    Updated celery tasks

--- babycenter/settings/base.py
+++ babycenter/settings/base.py
@@ -173,17 +173,9 @@ CELERYBEAT_SCHEDULE = {
         'task': 'molo.core.tasks.rotate_content',
         'schedule': crontab(minute=0),
     },
-    'demote_articles': {
-        'task': 'molo.core.tasks.demote_articles',
-        'schedule': crontab(minute="*/3"),
-    },
-    'promote_articles': {
-        'task': 'molo.core.tasks.promote_articles',
-        'schedule': crontab(minute="*/3"),
-    },
-    'publish_pages': {
-        'task': 'molo.core.tasks.publish_scheduled_pages',
-        'schedule': crontab(minute='*/3'),
+    'molo_consolidated_minute_task': {
+        'task': 'molo.core.tasks.molo_consolidated_minute_task',
+        'schedule': crontab(minute='*'),
     },
 }
 
